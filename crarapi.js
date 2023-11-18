9b5692c17c45fddd78c99046da23ab6940434dd0
Alejandro <diegoalejandrino1234@gmail.com>
2023-11-17 22:45:55 -0500

    crarapi.js

--- crarapi.js
+++ crarapi.js
@@ -1,30 +1,42 @@
 const http = require('http');
-const fs= require ('fs')
+const fs = require('fs')
+let users = [
+  {
+    name: "Alice",
+    age: 25,
+    email: "alice@example.com",
+    city: "New York"
+  },
+  {
+    name: "Bob",
+    age: 30,
+    email: "bob@example.com",
+    city: "San Francisco"
+  },
+  {
+    name: "Charlie",
+    age: 28,
+    email: "charlie@example.com",
+    city: "Los Angeles"
+  }
+];
 const PORT = 3001;
 // request
-const server = http.createServer((req, res) =>{
+const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    const nombre = req.url.split("?")[0]
-    const edad = req.url.split("?")[1]
-    const booleano = req.url.split("?")[2]
-    console.log(nombre)
-    console.log(edad)
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    if (edad >= 18 && booleano == "si") {
-      fs.appendFileSync("nombre.txt", nombre)
-    }else if (edad < 18){
-        fs.appendFileSync("nombre.txt", nombre)
-        
-      }
-      if (edad >= 18 && booleano == "si") {
-        res.end ("hola " + nombre  + "eres mayor de edad");
-      }else if (edad < 18){
-          res.end ("hola " + nombre  + "eres menor de edad");
-    } 
+    const name = req.url.split('?')[1];
+    const usuarios = users.find(x => x.name === name);
+    if (usuarios) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(usuarios));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('User no encontrado\n');
     }
-})
+  }
 
+
+});
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
